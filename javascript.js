3b3ebc31258d372cfb2caef5f76a7ea2b897c757
caffeinatedjoe <36642798+caffeinatedjoe@users.noreply.github.com>
2018-02-22 21:53:03 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,33 @@
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1hNxU4YCmZZ5uRfq8_eHUwLspzNWncJzmdclrzYAGlvM/pubhtml';
 function init() {
- Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1hNxU4YCmZZ5uRfq8_eHUwLspzNWncJzmdclrzYAGlvM/pubhtml',
- callback: function(data, tabletop) { 
- console.log(data)
- },
- simpleSheet: true } )
+	Tabletop.init( {
+		key: publicSpreadsheetUrl,
+		callback: showInfo,
+		simpleSheet: true
+	} )
 }
-window.addEventListener(‘DOMContentLoaded’, init)
+
+function showInfo(data, tabletop) {
+	alert('Successfully processed!')
+	console.log(data);
+}
+
+function makeTableHTML(myArray) {
+	var result = "<table border=1>";
+	for(var i=0; i<myArray.length; i++) {
+		result += "<tr>";
+		for(var j=0; j<myArray[i].length; j++){
+			result += "<td>"+myArray[i][j]+"</td>";
+		};
+		
+		result += "</tr>";
+	};
+	
+	result += "</table>";
+	return result;
+}
+
+window.addEventListener('DOMContentLoaded', init)
+      
+//$(‘.post’).append(makeTableHTML(data));
+console.log(makeTableHTML(data));
